d16bd650157acd710ba497264fc281a0f2e22e3f
JonasFOliveira <jff.oliveira2018@gmail.com>
2022-06-01 19:24:22 -0300

    Update

--- create.c
+++ create.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "create.h"
+#include "header.h"
 
 /*
     function that will create a matrix[n_rows * n_cols]
--- create.h
+++ create.h
@@ -1,6 +1,7 @@
 #ifndef CREATE_H
 #define CREATE_C
 
+
 typedef struct {
    int *data;
    int n_rows;
@@ -15,5 +16,7 @@ Matrix zeros_matrix(int n_rows, int n_cols);
 Matrix full_matrix(int n_rows, int n_cols, int values);
 Matrix i_matrix(int n);
 Matrix tile_matrix(Matrix matrix, int reps);
+int get_element(Matrix matrix, int ri, int ci);
+
 
 #endif
