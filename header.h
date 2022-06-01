b57938cf001f7b8ef66bd061459cad92723fdd2c
JonasFOliveira <jff.oliveira2018@gmail.com>
2022-06-01 19:25:50 -0300

    create a header main file

---
+++ header.h
@@ -0,0 +1,27 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef struct {
+   int *data;
+   int n_rows;
+   int n_cols;
+   int stride_rows;
+   int stride_cols;
+   int offset; 
+} Matrix;
+
+//======== Functions for create matrices ========
+
+Matrix create_matrix(int *data, int n_rows, int n_cols);
+Matrix zeros_matrix(int n_rows, int n_cols);
+Matrix full_matrix(int n_rows, int n_cols, int values);
+Matrix i_matrix(int n);
+Matrix tile_matrix(Matrix matrix, int reps);
+
+//======== Functions for acess matrices ========
+int get_element(Matrix matrix, int ri, int ci);
+void put_element(Matrix matrix, int ri, int ci, int elem);
+void print_matrix(Matrix matrix);
+
+
+#endif
