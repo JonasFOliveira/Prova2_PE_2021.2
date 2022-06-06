3c1c0bddad3c749f73561a65acb9a697602a3746
samgrs <samilagrodrigues@gmail.com>
2022-06-06 10:03:57 -0300

    update

--- aggregation.c
+++ aggregation.c
@@ -1,5 +1,10 @@
 #include "header.h"
 
+/*
+    function min returns the smallest element of the array.
+    > int matrix :  is ​​the matrix where the smallest element will be searched
+
+*/
 int min(Matrix matrix){
     int m = matrix.data[0];
     for(int i = 0; i < matrix.n_cols*matrix.n_rows; i++){
@@ -8,6 +13,12 @@ int min(Matrix matrix){
     }
     return m;
 }
+
+/*
+    function max returns the largest element of the array.
+    > int matrix :  is ​​the matrix where the smallest element will be searched
+
+*/
 int max(Matrix matrix){
     int m = matrix.data[0];
     for(int i = 0; i < matrix.n_cols*matrix.n_rows; i++){
@@ -16,6 +27,12 @@ int max(Matrix matrix){
     }
     return m;
 }
+
+/*
+    function argmin returns the index of the smallest element in the array.
+    > int matrix :  matrix is ​​the array where the index of the smallest element will be looked for
+
+*/
 int argmin(Matrix matrix){
     int m = min(matrix);
     int index = 0;
@@ -27,6 +44,12 @@ int argmin(Matrix matrix){
     }
     return index;
 }
+
+/*
+    function argmax returns the index of the largest element in the array.
+    > int matrix :  matrix is ​​the array where the index of the largest element will be looked for
+
+*/
 int argmax(Matrix matrix){
     int m = max(matrix);
     int index = 0;
